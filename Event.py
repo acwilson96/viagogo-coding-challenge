21b1b55047804ca66cd454146a66423c2eab665c
Alex Wilson <acwilson96@protonmail.com>
2017-09-15 14:18:29 +0100

    Commented.

--- Event.py
+++ Event.py
@@ -6,11 +6,13 @@ class Event:
         self.price      = price
         self.numTickets = numTickets
 
+    # Returns the Manhattan distance between the provided coordinate and this event.
     def manhattanDistanceTo(self, coord):
         event_x, event_y = self.coord
         x, y = coord
         return abs(event_x - x) + abs(event_y - y)
 
+    # Returns a string representation of this Event.
     def toString(self):
         x, y = self.coord
         return "Event " + str(self.id) + " - $" + str(self.price) + ", (" + str(x) + ", " + str(y) + ")"
