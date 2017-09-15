1fba57ef8a0d49482d7d2e9bda0e6b38a5eb53c8
Alex Wilson <acwilson96@protonmail.com>
2017-09-15 16:55:59 +0100

    Event will now contain a list of tickets.

--- Event.py
+++ Event.py
@@ -1,10 +1,10 @@
 class Event:
 
-    def __init__(self, id, coord, price, numTickets):
+    def __init__(self, id, coord, price, tickets):
         self.id         = id
         self.coord      = coord
         self.price      = price
-        self.numTickets = numTickets
+        self.tickets    = tickets
 
     # Returns the Manhattan distance between the provided coordinate and this event.
     def manhattanDistanceTo(self, coord):
