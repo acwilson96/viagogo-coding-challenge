d1774219ed82f4eda86e7834f8c8c4f45b46f41e
Alex Wilson <acwilson96@protonmail.com>
2017-09-15 15:42:40 +0100

    Created eventProvider, to host methods for providing events.

---
+++ eventProvider.py
@@ -0,0 +1,18 @@
+# Finds the k closest events to coords
+def closestEvents(k, eventsList, coords):
+
+    # Populate list of (distance, Event) pairs.
+    eventDistance = []
+
+    for event in eventsList:
+        # Get the distance from coords to the current Event.
+        distance = event.manhattanDistanceTo(coords)
+        eventDistance.append((distance, event))
+    
+    # Sort the list based on distance.
+    eventDistance = sorted(eventDistance, key=lambda x: x[1])
+
+    # Extract the first k elements.
+    output = eventDistance[:k]
+
+    return output
