127c60c812c4d42b083e75138280d03a4bddbfee
Alex Wilson <acwilson96@protonmail.com>
2017-09-15 15:48:46 +0100

    Fixed error in sorting.

--- eventProvider.py
+++ eventProvider.py
@@ -3,15 +3,24 @@ def closestEvents(k, eventsList, coords):
 
     # Populate list of (distance, Event) pairs.
     eventDistance = []
-
     for event in eventsList:
         # Get the distance from coords to the current Event.
         distance = event.manhattanDistanceTo(coords)
         eventDistance.append((distance, event))
     
-    # Sort the list based on distance.
-    eventDistance = sorted(eventDistance, key=lambda x: x[1])
+    for eventPair in eventDistance:
+            distance, event = eventPair
+            print(event.toString() + ", Distance " + str(distance))
 
+    # Sort the list based on distance.
+    eventDistance = sorted(eventDistance, key=lambda x: x[0])
+
+    print()
+    for eventPair in eventDistance:
+            distance, event = eventPair
+            print(event.toString() + ", Distance " + str(distance))
+
+    print()
     # Extract the first k elements.
     output = eventDistance[:k]
 
