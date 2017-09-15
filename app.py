3e84f824ac919e1ea4b2b022c446302b3bbf161d
Alex Wilson <acwilson96@protonmail.com>
2017-09-15 14:59:57 +0100

    Input validation and parsing.

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from seed import genWorld
 
+# Get our events.
 events = genWorld()
 
-for event in events:
-    print(event.toString())
+# Print instructions.
+print("\n\nEnter coordinates in form: x,y\n")
+print("---------------------------------")
 
+# Always run the CLI.
+while True:
+    # Capture the input.
+    userCoords = raw_input("\n\nPlease Input Coordinates:\n\n> ")
+    tokens = userCoords.split(',')
+
+    # Check correct format of coordinates provided.
+    if (len(tokens) < 2 or len(tokens) > 3):
+        print("\Enter coordinates in form: x,y\n")
+        print("------------------------------")
+    else:
+        try:
+            x_coord = int(tokens[0])
+            y_coord = int(tokens[1])
+            print("\nClosest Events to (" + str(x_coord) + ", " + str(y_coord) + "):")
+        except:
+            print("Input coordinates in form: x,y")
