ba309bb8b93623f777356c6a0cb3241ca8ac00a4
Alex Wilson <acwilson96@protonmail.com>
2017-09-15 18:20:31 +0100

    Ticket class now formats its own price.

--- Ticket.py
+++ Ticket.py
@@ -6,6 +6,10 @@ class Ticket:
         self.category = category
         self.price    = price
 
-        
+    # Makes sure that the price has trailing 0's if necessary.
+    def formatPrice(self, price):
+        output = format(price, ',.2f')
+        return str(output)
+
     def toString(self):
-        return self.category + " Ticket: " + str(self.price)
+        return self.category + " Ticket: $" + str(self.formatPrice(self.price))
