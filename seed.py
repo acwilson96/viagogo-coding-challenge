2f6ef9bb0f4a308401f79b21b91fa7bd9f28b90c
Alex Wilson <acwilson96@protonmail.com>
2017-09-16 03:03:21 +0100

    Slight change

--- seed.py
+++ seed.py
@@ -45,7 +45,7 @@ class World:
                 
         print('      -10 -9  -8  -7  -6  -5  -4  -3  -2  -1   0   1   2   3   4   5   6   7   8   9   10 \n')
         for line in lines:
-            print line
+            print(line)
         print('\n\n')
 
     # Function that generates a world model of Events; returns a list of Events.
